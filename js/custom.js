06476cd00793779321ba73fdb0d502114bc51017
viniciusfernandes47 <viniciussfernandes12@gmail.com>
2019-06-24 12:15:48 -0300

    final edit

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,73 @@ $('.listagem_depoimentos').slick({
     slidesToShow: 1,
     slidesToScroll: 1
   });
+
+  $(".agendar").click(function(){
+      $('.agendar_open').toggle("slow");
+  });
+
+  $(".previ").click(function(){
+    $("#carouselExampleIndicators").carousel("prev");
+  });
+
+  $(".nexti").click(function(){
+    $("#carouselExampleIndicators").carousel("next");
+  });
+
+  $(function() {                      
+    $(".carousel-bullets li").click(function() {  
+      $('.carousel-bullets li').removeClass("active");      
+      $(this).addClass("active");      
+    });
+  });
+
+  //Sticky header
+jQuery(function(){
+  createSticky(jQuery("header"));
+});
+
+function createSticky(sticky) {
+  if (typeof sticky != "undefined") {
+
+      var pos = sticky.offset().top ,
+          win = jQuery(window);
+
+      win.on("scroll", function() {
+
+          if( win.scrollTop() > pos ) {
+              sticky.addClass("stickyhead");
+          } else {
+              sticky.removeClass("stickyhead");
+          }           
+      });         
+  }
+}
+
+$(".hamburger").click(function()
+{
+    $(".navigation").toggleClass("open_menu");
+});
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+
+  $('li.nav-item a').removeClass('active');
+  $(this).addClass('active');
+  if( target.length ) {
+      
+      event.preventDefault();
+      var top = (target.offset().top) - 140;
+      $('html, body').stop().animate({
+          scrollTop: top
+      }, 1000);
+  }
+});   
+
+
+AOS.init();
+
+if($(window).height() > $("body").height()){
+  $("footer").css("position", "fixed");
+} else {
+  $("footer").css("position", "static");
+}
